3a423c0615669c27cb124db83e9c052be1673b62
Nicolas Severyns <contact@nicolasseveryns.be>
2024-04-16 22:39:09 +0200

    Try to get tooltips working. Should reverse order in html, to create l to r grid

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const actions = document.getElementById("actions");
 const menuButton = document.getElementById("menu-button");
-
+const roundButtons = document.getElementsByClassName("round-button");
 let flag = true;
 
 function showEl(el) {
@@ -24,10 +24,24 @@ function toggleStyle() {
   return;
 }
 
-function showToolTip() {
-  console.log("mouseover");
+function showToolTip(event) {
+  const target = event.target;
+  if (target.className === "round-button") {
+    const toolTipSpan = target.nextElementSibling;
+    toolTipSpan.style.display = "block";
+  }
+}
+
+function hideToolTip(event) {
+  const toolTips = document.getElementsByClassName("tooltip");
+  Array.from(toolTips).forEach((tooltip) => {
+    tooltip.style.display = "none";
+  });
 }
 
 actions.setAttribute("style", "display:none");
 actions.addEventListener("mouseover", showToolTip);
+Array.from(roundButtons).forEach((button) => {
+  button.addEventListener("mouseleave", hideToolTip);
+});
 menuButton.addEventListener("click", toggleStyle);
